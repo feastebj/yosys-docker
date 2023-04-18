95a2ff4bf2f821e23879d1c9bfb299add4737442
Benjamin Feaster <55043306+benipoo@users.noreply.github.com>
2023-04-18 01:09:46 -0400

    Update build2.sh

--- build2.sh
+++ build2.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-sudo docker pull benipoo/yosys:latest
-sudo docker run --name yosys -it --rm -v /home/ben/Documents/docker-stuff:/yosys/docker-stuff \benipoo/yosys:latest
+apt install npm -y && git clone https://github.com/nturley/netlistsvg
+cd netlistsvg
+npm install && npm install -g . && npm install elkjs --force
+cd examples
+yosys -p 'synth -auto-top ; abc -g cmos2,-NOR; write_verilog output.v' carry4bits.v carry4whole.v -p 'write_json answer.json'
+netlistsvg answer.json
