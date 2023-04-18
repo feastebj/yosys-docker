168b5abbed5af3057bcdb75b26648a6aa158c433
Benjamin Feaster <55043306+benipoo@users.noreply.github.com>
2023-04-18 00:43:05 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-sudo docker build -t yosys .
-sudo docker run --name yosys -it --rm -v /home/ben/Documents/docker-stuff:/yosys/docker-stuff \yosys:latest
-
+sudo docker pull benipoo/yosys:latest
+sudo docker run --name yosys -it --rm -v /home/ben/Documents/docker-stuff:/yosys/docker-stuff \benipoo/yosys:latest
